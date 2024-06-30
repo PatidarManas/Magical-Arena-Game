d022fcd49e59abe1a27a32c81745a77fd9d3fa55
Manas Patidar <manaspatidar170@gmail.com>
2024-06-30 12:08:04 +0530

    Updated the playerModal test file according to fixtures and added negative test cases

--- test/playerModal.test.js
+++ test/playerModal.test.js
@@ -1,62 +1,133 @@
-import { isEqual,isInRange, isTrue } from "../utils/assertions.js";
-import Player from "../src/playerModal.js";
+import { isEqual,isInRange, isTrue, throwsError } from "../utils/assertions.js";
+import { playerClassFixture } from "./testFixtures.js";
 
+// Postive Test Cases
 function testPlayerConstructor() {
-    // Function to test the values are assigned correctly
-    const player = new Player('test',20,5,3);
-
-    isEqual(player.health,20,'Health should be 20');
-    isEqual(player.strength,5,'Strength should be 20');
-    isEqual(player.attack,3,'Attack should be 20');
+    // Function to test the values are assigned as passed
+    console.log("Testing for health, strength and attack to be equal to user input");
+   
+    const player = playerClassFixture.positiveTestFixtures.validPlayerA;
+    
+    isEqual(player.health, 5, 'TestPlayerConstructor failed,consturctor failed at assigning health');
+    isEqual(player.strength,5,'TestPlayerConstructor failed, consturctor failed at assigning strength');
+    isEqual(player.attack,10,'TestPlayerConstructor failed, consturctor failed at assigning attack');
 
     console.log("testPlayerConstructor Passed.");
+    
 }
 
-function testPlayerRollMethods() {
+function testPlayerRoll() {
     // Function to test the attackImpact and defendImpact function 
-    const player = new Player('test',20,5,10);
+    console.log("Testing for attackImpact and defendImpact function to be in expected range");
+
+    const player = playerClassFixture.positiveTestFixtures.validPlayerB;
 
     const attackRollValue = player.attackImpact();
     const defendRollValue = player.defendImpact();
 
-    isInRange(attackRollValue,10,60,'Should be in range of 10 and 60');
-    isInRange(defendRollValue,5,30,'Should be in range of 5 and 30');
+    isInRange(attackRollValue,5,30,'TestPlayerRoll Failed: attackImpact Function Failed');
+    isInRange(defendRollValue,10,60,'TestPlayerRoll Failed:  defendImpact Function Failed');
 
-    console.log('testPlayerRollMethods Passed.')
+    console.log('testPlayerRoll Passed.');
 }
 
-function testDiceROll(){
+function testDiceRoll(){
     // Function to test rollDice funtion
-    const player = new Player('test',100,20,5);
+    console.log("Testing for rollDice Function to be in range of 1 to 6");
+
+    const player = playerClassFixture.positiveTestFixtures.validPlayerA;
     
     const rollValue = player.rollDice();
-    isInRange(rollValue,1,6,"Dice Value should be in between 1 and 6");
+    isInRange(rollValue,1,6,"TestDiceRoll Failed: rollDice function Failed");
 
     console.log('testDiceROll Passed.');
 }
 
 function testPlayerTakeDamage() {
-    // Function to check takeDamage and isAlive funtion
-    const player = new Player('test',100,20,5);
+    // Function to check takeDamage funtion
+    console.log("Testing for takeDamage Function to update health as expected");
+    const player = playerClassFixture.positiveTestFixtures.validPlayerB;
     
-    isTrue(player.isAlive(),"Player should be alive");
     player.takeDamage(30);
-    isEqual(player.health,70,"Player health should be 70");
+    isEqual(player.health,70,"TestPlayerTakeDamage Failed: takeDamage Failed");
     player.takeDamage(85);
-    isEqual(player.health,0,"Player health should be 0");
-    isTrue(!player.isAlive(),"Player should not be alive");
+    isEqual(player.health,0,"TestPlayerTakeDamage Failed: takeDamage Failed");
 
 
     console.log('testPlayerTakeDamage Passed.');
 }
 
+function testIsAlive() {
+    // Function to test isAlive function
+    console.log("Testing for isAlive Function to return Boolean as expected");
 
+    const alivePlayer = playerClassFixture.positiveTestFixtures.validPlayerA;
+    const deadPlayer = playerClassFixture.positiveTestFixtures.zeroHealthPlayer;
+
+    isTrue(alivePlayer.isAlive(),"TestIsAlive Failed: isAlive Failed for alive player");
+    isTrue(!deadPlayer.isAlive(),"TestIsAlive Failed: isAlive Failed for dead player");
+
+    console.log('testIsAlive Passed.');
+}
+
+// Negative Test Cases
+function testNegativeHealth() {
+    //Function to test constructor in case of Negative health 
+    console.log("Testing for player Initialization with negative health ");
+
+    const negativeHealthPlayer = playerClassFixture.negativeTestFixtures.negativeHealthPlayer;
+
+    throwsError(negativeHealthPlayer,"testNegativeHealth Failed");
+
+    console.log('testNegativeHealth Passed.');
+}
+
+function testNegativeStrength() {
+    //Function to test constructor in case of Negative strength 
+    console.log("Testing for player Initialization with negative strength ");
+
+    const negativeStrengthPlayer = playerClassFixture.negativeTestFixtures.negativeStrengthPlayer;
+
+    throwsError(negativeStrengthPlayer,"testNegativeStrength Failed");
+
+    console.log('testNegativeStrength Passed.');
+}
+
+function testNegativeAttack() {
+    //Function to test constructor in case of Negative attack 
+    console.log("Testing for player Initialization with negative attack ");
+
+    const negativeAttackPlayer = playerClassFixture.negativeTestFixtures.negativeAttackPlayer;
+
+    throwsError(negativeAttackPlayer,"testNegativeAttack Failed");
+
+    console.log('testNegativeAttack Passed.');
+}
+
+function testNegativeTakeDamage() {
+    // Function to test takeDamage function with invalid damage
+    console.log("Testing for takeDamage Function with negative value");
+
+    const negativeDamage = playerClassFixture.negativeTestFixtures.invalidDamage;
+    const player = playerClassFixture.positiveTestFixtures.validPlayerA;
+
+    throwsError(()=> player.takeDamage(negativeDamage),"TestNegativeTakeDamage Failed");
+
+    console.log('testNegativeTakeDamage Passed.');
+}
 function runTestFunctions() {
     // Function to run all test funtions
+    console.log("Runing all tests for Player Class");
     testPlayerConstructor();
-    testPlayerRollMethods();
-    testDiceROll();
+    testPlayerRoll();
+    testDiceRoll();
     testPlayerTakeDamage();
+    testIsAlive();
+    testNegativeAttack();
+    testNegativeHealth();
+    testNegativeStrength();
+    testNegativeTakeDamage();
+    console.log("\n All test cases Passed for Player Class");
 }
 
 runTestFunctions();
